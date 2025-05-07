80a18ac23a2d7065d7909c0333da583b448cbcf3
anto31ad <anto31.ad@gmail.com>
2025-05-07 23:34:26 +0200

    refactor

--- scripts/generate_logs.py
+++ scripts/generate_logs.py
@@ -19,40 +19,6 @@ class UserAction(Enum):
     EDIT = 'edit'
     NONE = 'none'
 
-
-# def generate_action_sequence(super_user: bool):
-
-#     if super_user:
-#         actions_weight = [
-#             0.05, # login
-#             0.35, # logout
-#             0.60, # edit
-#         ]
-#     else:
-#         actions_weight = [
-#             0.05, # login
-#             0.45, # logout
-#             0.50, # edit
-#         ]
-
-#     possible_actions = [member for member in UserAction]
-#     sequence = []
-#     time_deltas = []
-#     loop = True
-#     while loop:
-#         next_action = random.choices(possible_actions, weights=actions_weight, k=1)
-#         sequence.append(next_action)
-
-#         random_minutes = random.randint(0, 15)
-#         time_deltas.append(timedelta(minutes=random_minutes))
-        
-#         loop = next_action != UserAction.LOGOUT
-
-#     return {
-#         sequence: sequence,
-#         time_deltas: time_deltas
-#     }
-
 def generate_realistic_events(target_num_of_items: int):
     current_datetime = datetime.now()
 
@@ -97,14 +63,15 @@ def generate_realistic_events(target_num_of_items: int):
             action=UserAction.LOGOUT
         )
 
+
 def pick_next_super_user_action(online: bool):
     if not online:
         return UserAction.LOGIN
     # else
     perc = random.random()
-    if perc < 0.05:
+    if perc < 0.01:
         return UserAction.LOGIN
-    elif perc < 0.40: # 35%
+    elif perc < 0.30:
         return UserAction.LOGOUT
     elif perc < 0.80:
         return UserAction.EDIT
@@ -116,11 +83,11 @@ def pick_next_unpriviledged_user_action(online: bool):
         return UserAction.LOGIN
     # else
     perc = random.random()
-    if perc < 0.05: # 5%
+    if perc < 0.01:
         return UserAction.LOGIN
-    elif perc < 0.10: # 5%
+    elif perc < 0.06:
         return UserAction.EDIT
-    elif perc < 0.40: # 30%
+    elif perc < 0.50:
         return UserAction.LOGOUT
     else:
         return UserAction.NONE
@@ -144,3 +111,37 @@ with open(TEST_LOG_PATH.resolve(), mode="w", newline="") as file:
         count += 1
 
 print(f"Generated {count} rows")
+
+
+# def generate_action_sequence(super_user: bool):
+
+#     if super_user:
+#         actions_weight = [
+#             0.05, # login
+#             0.35, # logout
+#             0.60, # edit
+#         ]
+#     else:
+#         actions_weight = [
+#             0.05, # login
+#             0.45, # logout
+#             0.50, # edit
+#         ]
+
+#     possible_actions = [member for member in UserAction]
+#     sequence = []
+#     time_deltas = []
+#     loop = True
+#     while loop:
+#         next_action = random.choices(possible_actions, weights=actions_weight, k=1)
+#         sequence.append(next_action)
+
+#         random_minutes = random.randint(0, 15)
+#         time_deltas.append(timedelta(minutes=random_minutes))
+        
+#         loop = next_action != UserAction.LOGOUT
+
+#     return {
+#         sequence: sequence,
+#         time_deltas: time_deltas
+#     }
