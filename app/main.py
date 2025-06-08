7523466cc73ebe2a0eb9fe135947c119d1124e81
anto31ad <anto31.ad@gmail.com>
2025-06-08 16:22:22 +0200

    feat: select operations by typing

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from pyswip import Prolog
 
 from .test import generate_events_to_file
 from .control import monitor_manifesto, train
-from .learning import get_user_predictive_models
+from .learning import get_user_predictors
 
 from .paths import (
     BASE_RULES_PATH,
@@ -13,24 +13,32 @@ from .paths import (
 
 def main_menu_loop(prolog):
 
-    predictors = get_user_predictive_models()
-    print("Choose next operation >")
+    predictors = get_user_predictors()
 
     while True:
-        print("  (1) Start monitoring events")
-        print(f"  (2) Train an anomaly predictor for each user based on past events (reads from '{EVENT_EXAMPLES_PATH}')")
-        print(f"  (3) Test the generator of events (writes to '{TEST_LOG_PATH}')")
-        print("  (q) quit")
+        print("Choose next operation by typing (what's inside) :")
+        print("  (s)    Start monitoring events")
+        print(f"  (t)    Train an anomaly predictor for each user based on past events (reads from '{EVENT_EXAMPLES_PATH}')")
+        print(f"  (tdel) Delete predictors")
+        print(f"  (gen)  Test the generator of events (writes to '{TEST_LOG_PATH}')")
+        print(f"  (rst)  Delete past events data (deletes file '{EVENT_EXAMPLES_PATH}')")
+        print("  (q)    quit")
 
         input_value = input().lower()
-        if input_value == '1':
+        if input_value == 's':
             monitor_manifesto(prolog, predictors)
             break
-        elif input_value == '2':
+        elif input_value == 't':
             train()
-            predictors = get_user_predictive_models()
+            predictors = get_user_predictors()
             break
-        elif input_value == '3':
+        elif input_value == 'tdel':
+            # TODO delete
+            break
+        elif input_value == 'rst':
+            # TODO delete events
+            break
+        elif input_value == 'gen':
             generate_events_to_file(10)
             break
         elif input_value == 'q':
